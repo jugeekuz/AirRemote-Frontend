98d8a84ee963ffe15c0990b8089952aedd6bdf97
Anastasios Diamantis <tdiamantis97@gmail.com>
2024-11-04 20:16:10 +0200

    Miscellaneous aesthetics

--- src/components/NoticeBox.jsx
+++ src/components/NoticeBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BadgeAlert } from 'lucide-react'
 const NoticeBox = ({children}) => {
     return (
-        <div className="flex w-full min-h-16 p-1 pr-2 -mb-2 -mt-1">
+        <div className="flex w-full min-h-12 p-1 pr-2 -mt-1">
             <div className="flex w-full h-full items-center justify-start rounded-md bg-green-100 bg-opacity-50 border-2 border-green-500 py-1">
                 <div className="h-full"><BadgeAlert className={"ml-2 mr-2"} size={"23px"} fill="#22c55e" color='#ffffff'/></div>
                 <span className="text-xs font-medium text-gray-700">{children}</span>
