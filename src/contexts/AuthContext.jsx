0d62f2eee904522a664f6b0fedda1c4a89f7bd30
Anastasios Diamantis <tdiamantis97@gmail.com>
2024-11-08 17:28:34 +0200

    Bug Fix

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -21,6 +21,7 @@ export const AuthProvider = ({ children }) => {
     const username = useRef("User");
     const decodeIdToken = (token) => {
         try {
+            if (!token) return;
             const base64Url = token.split('.')[1];
             const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
             const jsonPayload = decodeURIComponent(
